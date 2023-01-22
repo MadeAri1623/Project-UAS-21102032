f18f3522c1c90066a364bbcdae331bee808b1368
MadeAri1623 <madeariputrawan@gmail.com>
2023-01-22 22:51:58 +0800

    first commit

---
+++ resources/views/code.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.default')
+
+@section('content')
+<section>
+    <div class="conttainer mt-5">
+        <div class="row">
+            <div class="col-lg-8">
+<h3>CODINGAN PROJECT</h3>
+</div>
+          <div class ="col-lg-55">
+          <table class="table table-bordered border-primary">
+            <tr>
+            <center><img src="img/11.png" width="800px" ></center>
+           
+
+            </tr>
+                    
+                    
+</section>
+@endsection
